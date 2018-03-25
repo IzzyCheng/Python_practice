c1c0ddab0323b049effdf7d784f4ab133e87235b
Izzy <qaz30239@gmail.com>
2018-03-25 13:07:54 +0800

    backup

--- pydicom.py
+++ pydicom.py
@@ -19,10 +19,10 @@ data = ds.pixel_array[0]
 for i in range(1, 256, 1):
     data = numpy.hstack((data, ds.pixel_array[i]))
 
-#count the number of pixel value at x=60~210, y=50~170(dicom2); x=85~165, y=95~160(dicom0)
+#count the number of pixel value at x=100~170, y=100~170
 numb = numpy.zeros(1200)
-for x in range(60, 211, 1):
-    for y in range(50, 171, 1):
+for x in range(100, 161, 1):
+    for y in range(100, 161, 1):
         numb[ds.pixel_array[y][x]] +=1
 
 #count 10(1%) numb data into a bar
@@ -33,8 +33,14 @@ for i in range(0, length, 1):
     for j in range(0, width, 1):
         bar[i] += numb[i*width+j]
 
-        
+
+volume = [1169, 1091, 1011, 903, 811, 747, 709, 705, 739, 859, 979, 1052, 1121, 1151, 1163, 1154, 1171, 1223, 1247, 1217]
+volume = numpy.tile(volume, 3)
+plt.plot(volume)
+plt.show()
+
 #print the two chart
+'''
 numb_x = 300
 numb_y = 100
 f, axarr = plt.subplots(2)
@@ -47,6 +53,7 @@ axarr[1].axis([0, numb_x/width, 0, numb_y*width])
 axarr[1].set_title('Sum every '+str(width)+' data')
 axarr[1].set_ylabel('count')
 plt.show()
+'''
 
 #find start and end of white range
 start=0
@@ -64,12 +71,11 @@ for i in range(15, 119, 1):    #find the max of rest bar
     if bar[i]>second_top:
         second_top=bar[i]
         index = i
-for i in range(index, start-1, -1):
-    if bar[i] < bar[i-1]:
-        end = i
-        break
-    else:
-        end = -1
+min = bar[index]
+for i in range(index, start-1, -1): #find the min between two peak
+    if bar[i-1]<min:
+        min = bar[i-1]
+        end = i-1
 
 if start >0 and end >0:
     print(start, end)
@@ -79,6 +85,8 @@ else:
     exit()
 
 #draw to white(1000)
+start = 4
+end = 13
 for y in range(0, 256, 1):
     for x in range(0, 256, 1):
         if ds.pixel_array[x][y] >= start*10 and ds.pixel_array[x][y]<= (end+1)*10:
@@ -86,21 +94,23 @@ for y in range(0, 256, 1):
 pylab.imshow(ds.pixel_array, cmap=pylab.cm.bone)
 pylab.show()
 
+#find edge and count volume
 e=0
 p=0
+volume=0
 edge_list = numpy.zeros((400, 2))
 passed = numpy.zeros((3000, 2))
-
 def getedge(x, y):
-    global e, p
+    global e, p, volume
     exist = 0
     for i in range(0, 3000, 1):
         if passed[i][0] == x and passed[i][1] == y:
             exist = 1
-    if exist != 1:
+    if exist != 1:      #haven't pass
         passed[p][0] = x
         passed[p][1] = y
         p+=1
+        volume+=1
         if (ds.pixel_array[x-1][y]<1000 and ds.pixel_array[x+1][y]<1000 and ds.pixel_array[x][y-1]<1000 and ds.pixel_array[x][y+1]<1000 and \
         ds.pixel_array[x-1][y-1]<1000 and ds.pixel_array[x-1][y+1]<1000 and ds.pixel_array[x+1][y-1]<1000 and ds.pixel_array[x+1][y+1]<1000):
             getedge(x-1, y)
@@ -111,9 +121,9 @@ def getedge(x, y):
             getedge(x-1, y+1)
             getedge(x+1, y-1)
             getedge(x+1, y+1)
-        else:
+        else:           #touch the edge
             exist = 0
-            for i in range(0, 400, 1):
+            for i in range(0, 400, 1):      #double check edge
                 if edge_list[i][0] == x and edge_list[i][1] == y:
                     exist = 1
             if exist != 1:
@@ -121,9 +131,10 @@ def getedge(x, y):
                 edge_list[e][1] = y
                 e+=1
 
-'''
+
 sys.setrecursionlimit(10000)
-getedge(120, 150)
+getedge(140, 130)
+
 
 temp = "[[%d, %d]" %(edge_list[0][0], edge_list[0][1])
 print(temp)
@@ -132,6 +143,6 @@ for i in range(1, 399, 1):
     print(temp)
 temp = ",[%d, %d]]" %(edge_list[399][0], edge_list[399][1])
 print(temp)
-'''
+print(volume)
 
 
